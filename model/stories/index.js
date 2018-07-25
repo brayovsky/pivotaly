ae697d33dfd10e7e794c1f8326a0c2e51309c0c9
Brian Muthui <brian.muthui@andela.com>
2018-07-25 13:49:59 +0300

    Promisify api call

--- model/stories/index.js
+++ model/stories/index.js
@@ -1,13 +1,27 @@
 const {pivotalTracker} = require("../common")
 const {common} = require("../../lib/commands/common")
 
-exports.getStory = function(context, storyID, callback, fields = []) {
+exports.getStory = function(context, storyID, fields = []) {
   if(! Array.isArray(fields)) fields = String(fields).split(",")
   const options = {
-    path: `/services/v5/projects/${context.globalState.get(common.globals.projectID)}/stories/${storyID}?fields=${fields.join()}`,
+    path: `/services/v5/projects/${context.workspaceState.get(common.globals.projectID)}/stories/${storyID}?fields=${fields.join()}`,
     headers: {
       "X-TrackerToken": `${context.globalState.get(common.globals.APItoken)}`
     }
   }
-  pivotalTracker.get(options, callback)
+
+  return new Promise(function (resolve, reject) {
+    pivotalTracker.get(options, function (err, req, res, data){
+      if(err)
+        reject({
+          res,
+          err
+        })
+      else
+        resolve({
+          res,
+          data
+        })
+    })
+  })
 }
