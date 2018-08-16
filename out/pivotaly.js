0619944bb9742190a6f521a98300bb53d6771883
Brian Muthui <brian.muthui@andela.com>
2018-08-16 12:24:11 +0300

    Add non-repo validation - Migrate state within activate

--- out/pivotaly.js
+++ out/pivotaly.js
@@ -1,33 +1,43 @@
-const vscode = require("vscode")
+const {workspace, commands} = require("vscode")
 const {createPTStatusBarItem} = require("../lib/pivotaly/createPTStatusBarItem")
 const { commandRepo } = require("../lib/commands")
 const {validate, validateStory} = require("../lib/validation/validate")
 const path = require("path")
-const githook = require("git-emit")(path.join(vscode.workspace.rootPath, ".git"), onEmitError)
+const gitEmit = require("git-emit")
+const isRepo = require("../lib/validation/validators/isRepo")
+const {setState} = require("../lib/helpers/pivotaly")
 
-function activate(context) {
+async function activate(context) {
+  const isARepo = await isRepo(workspace.rootPath)
   let PTStatusBarItem = createPTStatusBarItem()
 
-  let pendingStart = vscode.commands.registerCommand(commandRepo.commands.storyState.startStory, () => commandRepo.startStory(context))
-  let pendingStop = vscode.commands.registerCommand(commandRepo.commands.storyState.stopStory, () => commandRepo.stopStory(context))
-  let pendingFinish = vscode.commands.registerCommand(commandRepo.commands.storyState.finishStory, () => commandRepo.finishStory(context))
-  let pendingDeliver = vscode.commands.registerCommand(commandRepo.commands.storyState.deliverStory, () => commandRepo.deliverStory(context))
-  let pendingLink = vscode.commands.registerCommand(commandRepo.commands.workState.linkStory, () => commandRepo.linkStory(context))
-  let pendingCommandPick = vscode.commands.registerCommand(commandRepo.commands.internal.showCommandsQuickPick, () => commandRepo.showAllCommands(context))
-  let tokenRegistration = vscode.commands.registerCommand(commandRepo.commands.internal.registerToken, () => commandRepo.registerToken(context))
-  let projectIDRegistration = vscode.commands.registerCommand(commandRepo.commands.internal.registerProjectID, () => commandRepo.registerProjectID(context))
+  let pendingStart = commands.registerCommand(commandRepo.commands.storyState.startStory, () => commandRepo.startStory(context))
+  let pendingStop = commands.registerCommand(commandRepo.commands.storyState.stopStory, () => commandRepo.stopStory(context))
+  let pendingFinish = commands.registerCommand(commandRepo.commands.storyState.finishStory, () => commandRepo.finishStory(context))
+  let pendingDeliver = commands.registerCommand(commandRepo.commands.storyState.deliverStory, () => commandRepo.deliverStory(context))
+  let pendingLink = commands.registerCommand(commandRepo.commands.workState.linkStory, () => commandRepo.linkStory(context))
+  let pendingCommandPick = commands.registerCommand(commandRepo.commands.internal.showCommandsQuickPick, () => commandRepo.showAllCommands(context))
+  let tokenRegistration = commands.registerCommand(commandRepo.commands.internal.registerToken, () => commandRepo.registerToken(context))
+  let projectIDRegistration = commands.registerCommand(commandRepo.commands.internal.registerProjectID, () => commandRepo.registerProjectID(context))
 
   validate("token", context, true).then((res) => {
     validate("projectID", context, true).then((res) => {
-      validateStory(context)
+      if (isARepo) validateStory(context)
+      else validate("story", context)
     })
   })
+  
+  if(isARepo){
+    const gitHook = gitEmit(path.join(workspace.rootPath, ".git"))
+    gitHook.on("post-checkout", () => {
+      validateStory(context)
+    })
+  }
 
-  githook.on("post-checkout", () => {
-    validateStory(context)
-  })
+  // run state migrations ;P
+  setState(context)
 
-  // dispose
+  // disposables
   context.subscriptions.push(PTStatusBarItem, pendingStart, pendingStop, pendingFinish, pendingDeliver, pendingLink, pendingCommandPick, tokenRegistration, projectIDRegistration);
 }
 exports.activate = activate;
@@ -35,12 +45,6 @@ exports.activate = activate;
 
 function deactivate() {
   // clean up
-}
-
-function onEmitError(err, emitter) {
-  if(err){
-    vscode.window.showErrorMessage("No git repository detected. Pivotaly will not work!!")
-  }
 }
 
 exports.deactivate = deactivate;
