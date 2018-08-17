f22047b44d69588d0d75d131aeff27e4d095bbd4
Brian Muthui <brian.muthui@andela.com>
2018-08-17 13:14:20 +0300

    Refactor - Clean up code

--- out/pivotaly.js
+++ out/pivotaly.js
@@ -12,16 +12,17 @@ async function activate(context) {
   const isARepo = await isRepo(workspace.rootPath)
   context.workspaceState.update(common.globals.isARepo, isARepo)
 
-  let PTStatusBarItem = createPTStatusBarItem()
-
-  let pendingStart = commands.registerCommand(commandRepo.commands.storyState.startStory, () => commandRepo.startStory(context))
-  let pendingStop = commands.registerCommand(commandRepo.commands.storyState.stopStory, () => commandRepo.stopStory(context))
-  let pendingFinish = commands.registerCommand(commandRepo.commands.storyState.finishStory, () => commandRepo.finishStory(context))
-  let pendingDeliver = commands.registerCommand(commandRepo.commands.storyState.deliverStory, () => commandRepo.deliverStory(context))
-  let pendingLink = commands.registerCommand(commandRepo.commands.workState.linkStory, () => commandRepo.linkStory(context))
-  let pendingCommandPick = commands.registerCommand(commandRepo.commands.internal.showCommandsQuickPick, () => commandRepo.showAllCommands(context))
-  let tokenRegistration = commands.registerCommand(commandRepo.commands.internal.registerToken, () => commandRepo.registerToken(context))
-  let projectIDRegistration = commands.registerCommand(commandRepo.commands.internal.registerProjectID, () => commandRepo.registerProjectID(context))
+  context.subscriptions.push(
+    createPTStatusBarItem(),
+    commands.registerCommand(commandRepo.commands.storyState.startStory, () => commandRepo.startStory(context)),
+    commands.registerCommand(commandRepo.commands.storyState.stopStory, () => commandRepo.stopStory(context)),
+    commands.registerCommand(commandRepo.commands.storyState.finishStory, () => commandRepo.finishStory(context)),
+    commands.registerCommand(commandRepo.commands.storyState.deliverStory, () => commandRepo.deliverStory(context)),
+    commands.registerCommand(commandRepo.commands.workState.linkStory, () => commandRepo.linkStory(context)),
+    commands.registerCommand(commandRepo.commands.internal.showCommandsQuickPick, () => commandRepo.showAllCommands(context)),
+    commands.registerCommand(commandRepo.commands.internal.registerToken, () => commandRepo.registerToken(context)),
+    commands.registerCommand(commandRepo.commands.internal.registerProjectID, () => commandRepo.registerProjectID(context))
+  )
 
   validate("token", context, true).then((res) => {
     validate("projectID", context, true).then((res) => {
@@ -39,9 +40,6 @@ async function activate(context) {
 
   // run state migrations ;P
   setState(context)
-
-  // disposables
-  context.subscriptions.push(PTStatusBarItem, pendingStart, pendingStop, pendingFinish, pendingDeliver, pendingLink, pendingCommandPick, tokenRegistration, projectIDRegistration);
 }
 exports.activate = activate;
 
