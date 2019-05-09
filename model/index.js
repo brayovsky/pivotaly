d0d3b4dc92e932f5acfe46e07d56602d29b4506f
Brian Muthui <brian.muthui@andela.com>
2019-05-09 19:39:21 +0300

    Add procedures for requesting token and project id

--- model/index.js
+++ model/index.js
@@ -1,7 +1,7 @@
 const clients = require('restify-clients')
 const {common} = require('../lib/commands/common')
 const {normaliseFields} = require('../lib/adapters/normaliseFields')
-const rebound = require('../lib/validation/rebounds')
+const requestToken = require('../lib/procedures/requestToken')
 
 class Model {
   constructor(context) {
@@ -21,7 +21,7 @@ class Model {
       (err, req, res, data) => {
         if(err){
           if(err.statusCode === 403 && err.restCode === 'invalid_authentication')
-            rebound('token', this._context)
+            requestToken('Invalid token detected', [this._context])
           return reject(err.restCode)
         }
         resolve({res, data})
@@ -31,15 +31,16 @@ class Model {
 
   _update(method, path, updateData) {
     method = method.toLowerCase()
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
       this._pivotalTrackerClient[method]({path, headers: {
         'X-TrackerToken': this._token
       }},
       updateData,
       (err, req, res, data) => {
         if(err){
-          resolve({res})
-          return
+          if(err.statusCode === 403 && err.restCode === 'invalid_authentication')
+            requestToken('Invalid token detected', [this._context])
+          return reject(err.restCode)
         }
         resolve({res, data})
       })
