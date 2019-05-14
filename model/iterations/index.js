d5d11643499a9f4bd08c8523068c8d8b9ee2cb90
Brian Muthui <brian.muthui@andela.com>
2019-05-15 02:10:21 +0300

    Add maxIterations to Iterations constructor - Add parameters usin _addParams function

--- model/iterations/index.js
+++ model/iterations/index.js
@@ -1,19 +1,21 @@
 const Model = require('../')
 
 class PtIterations extends Model{
-  constructor(context) {
+  constructor(context, maxIterations) {
     super(context)
     this._baseIterationsPath = `${this._baseApiPath}/iterations`
+    this._maxIterations = maxIterations
   }
 
   get _endpoints() {
     return {
-      getIterations: scope => `${this._baseIterationsPath}?scope=${scope}`,
+      getIterations: scope => this._addParams(this._baseIterationsPath, {scope, offset: (0 - this._maxIterations), limit: this._maxIterations + 1}),
       getIterationCycleTime: iterationNumber => `${this._baseIterationsPath}/${iterationNumber}/analytics/cycle_time_details`
     }
   }
 
-  getIterations(scope = '') {
+  getIterations(scope = 'done_current') {
+    if(!['done', 'current', 'backlog', 'current_backlog', 'done_current'].includes(scope)) throw new Error('Invalid scope')
     return this._fetch('get', this._endpoints.getIterations(scope))
   }
 
