d80653015415d0627b0d41c8849de3f68fac554e
Brian Muthui <brian.muthui@andela.com>
2019-05-15 22:10:35 +0300

    Fix iteration model tests

--- model/iterations/index.spec.js
+++ model/iterations/index.spec.js
@@ -4,7 +4,7 @@ const Model = require('../')
 const Context = require('../../test/factories/vscode/context')
 
 const context = Context.build()
-const IterationResource = new PtIteration(context, 30)
+const IterationResource = new PtIteration(context)
 
 
 describe('#iterations', () => {
@@ -17,8 +17,18 @@ describe('#iterations', () => {
     test('it should have all iteration endpoints', () => {
       const endpoints = IterationResource._endpoints
       expect(endpoints).toHaveProperty('getIterations')
+      expect(endpoints).toHaveProperty('getDoneIterations')
       expect(endpoints).toHaveProperty('getIterationCycleTime')
-      endpoints.getIterations('done')
+      endpoints.getIterations({scope: 'current', limit: 10})
+      expect(modelMockInstance._addParams).toHaveBeenCalledTimes(1)
+      expect({
+        scope: 'current',
+        limit: 10
+      }).toMatchObject(modelMockInstance._addParams.mock.calls[0][1]);
+
+      modelMockInstance._addParams.mockClear()
+
+      endpoints.getDoneIterations('done', 30)
       expect(modelMockInstance._addParams).toHaveBeenCalledTimes(1)
       expect(modelMockInstance._addParams.mock.calls[0][0]).toEqual(IterationResource._baseIterationsPath);
       expect({
@@ -38,11 +48,34 @@ describe('#iterations', () => {
     afterEach(() => Model.mockClear())
 
     test('it should raise error if scope is invalid', () => {
-      expect(() => IterationResource.getIterations('invalid')).toThrowError('Invalid scope')
+      expect(() => IterationResource.getIterations({scope: 'done'})).toThrowError('Invalid scope')
     })
 
     test('it should call fetch with the right params', async () => {
       await IterationResource.getIterations('current')
+      expect(modelMockInstance._fetch).toHaveBeenCalledWith('get', 'apiPath?params=7')
+    })
+  })
+
+  describe('getDoneIterations', () => {
+    const modelMockInstance = Model.mock.instances[0]
+    modelMockInstance._addParams = jest.fn().mockReturnValue('apiPath?params=7')
+
+    afterEach(() => Model.mockClear())
+
+    test('it should call getIterations if scope does not include done', () => {
+      IterationResource.getIterations = jest.fn()
+      IterationResource.getDoneIterations('current')
+      expect(IterationResource.getIterations).toHaveBeenCalledTimes(1)
+    })
+
+    test('it should raise error if scope is invalid', () => {
+      expect(() => IterationResource.getDoneIterations('invalid')).toThrowError('Invalid scope')
+    })
+
+
+    test('it should call fetch with the right params', async () => {
+      await IterationResource.getDoneIterations('done')
       expect(modelMockInstance._fetch).toHaveBeenCalledWith('get', 'apiPath?params=7')
     })
   })
