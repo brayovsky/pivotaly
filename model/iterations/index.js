7d79e77de886a184f74609e6e7b5a1290e3f6e58
Brian Muthui <brian.muthui@andela.com>
2019-05-15 21:23:53 +0300

    Add getDoneIterations method

--- model/iterations/index.js
+++ model/iterations/index.js
@@ -8,14 +8,21 @@ class PtIterations extends Model{
 
   get _endpoints() {
     return {
-      getIterations: (scope, maxIterations) => this._addParams(this._baseIterationsPath, {scope, offset: (0 - maxIterations), limit: maxIterations + 1}),
+      getDoneIterations: (scope, maxIterations) => this._addParams(this._baseIterationsPath, {scope, offset: (0 - maxIterations), limit: maxIterations + 1}),
+      getIterations: params => this._addParams(this._baseIterationsPath, params),
       getIterationCycleTime: iterationNumber => `${this._baseIterationsPath}/${iterationNumber}/analytics/cycle_time_details`
     }
   }
 
-  getIterations(scope = 'done_current', maxIterations) {
+  getDoneIterations(scope = 'done_current', maxIterations) {
     if(!['done', 'current', 'backlog', 'current_backlog', 'done_current'].includes(scope)) throw new Error('Invalid scope')
-    return this._fetch('get', this._endpoints.getIterations(scope, maxIterations))
+    if(!scope.includes('done')) return this.getIterations({scope})
+    return this._fetch('get', this._endpoints.getDoneIterations(scope, maxIterations))
+  }
+
+  getIterations(params) {
+    if(params.scope && !['current', 'backlog', 'current_backlog'].includes(params.scope)) throw new Error('Invalid scope')
+    return this._fetch('get', this._endpoints.getIterations(params))
   }
 
   getIterationCycleTime(iterationNumber) {
