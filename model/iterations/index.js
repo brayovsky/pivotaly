01fb74835585df26c1c47aaf5044042abbd013c9
Brian Muthui <brian.muthui@andela.com>
2019-05-15 18:09:48 +0300

    Refactor PtIterations class to not require maxIterations

--- model/iterations/index.js
+++ model/iterations/index.js
@@ -1,22 +1,21 @@
 const Model = require('../')
 
 class PtIterations extends Model{
-  constructor(context, maxIterations) {
+  constructor(context) {
     super(context)
     this._baseIterationsPath = `${this._baseApiPath}/iterations`
-    this._maxIterations = maxIterations
   }
 
   get _endpoints() {
     return {
-      getIterations: scope => this._addParams(this._baseIterationsPath, {scope, offset: (0 - this._maxIterations), limit: this._maxIterations + 1}),
+      getIterations: (scope, maxIterations) => this._addParams(this._baseIterationsPath, {scope, offset: (0 - maxIterations), limit: maxIterations + 1}),
       getIterationCycleTime: iterationNumber => `${this._baseIterationsPath}/${iterationNumber}/analytics/cycle_time_details`
     }
   }
 
-  getIterations(scope = 'done_current') {
+  getIterations(scope = 'done_current', maxIterations) {
     if(!['done', 'current', 'backlog', 'current_backlog', 'done_current'].includes(scope)) throw new Error('Invalid scope')
-    return this._fetch('get', this._endpoints.getIterations(scope))
+    return this._fetch('get', this._endpoints.getIterations(scope, maxIterations))
   }
 
   getIterationCycleTime(iterationNumber) {
