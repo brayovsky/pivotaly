24bb194ea4f3320b97e937977f272b9355c4d9a9
Brian Muthui <brian.muthui@andela.com>
2019-05-17 20:45:25 +0300

    Refresh memnber cycle time data after changing project or token

--- out/pivotaly.js
+++ out/pivotaly.js
@@ -26,7 +26,7 @@ const activate = async context => {
 
   const cycleTimeProvider = new CycleTimeDataProvider(context, 6, 'done_current')
   const storyInfoProvider = new StoryInfoDataProvider(context)
-  const cpProvider = new ControlPanelDataProvider(context, statusBarItem)
+  const cpProvider = new ControlPanelDataProvider(context, storyInfoProvider, cycleTimeProvider)
 
   context.subscriptions.push(
     statusBarItem,
@@ -43,6 +43,7 @@ const activate = async context => {
     commands.registerCommand(commandRepo.commands.internal.registerProjectID, () => commandRepo.registerProjectID(context, storyInfoProvider)),
     commands.registerCommand(commandRepo.commands.statistics.cycleTime, (context, iteration) =>  commandRepo.showStats(context, iteration)),
     commands.registerCommand(commandRepo.commands.storyState.refreshStateView, () => commandRepo.refreshStateView(context, storyInfoProvider)),
+    commands.registerCommand(commandRepo.commands.storyState.refreshMemberCycleView, () => commandRepo.refreshMemberCycleView(cycleTimeProvider)),
     commands.registerCommand(commandRepo.commands.storyState.deliverTask, taskTreeeItem => commandRepo.deliverTask(taskTreeeItem, context)),
     commands.registerCommand(commandRepo.commands.storyState.unDeliverTask, taskTreeItem => commandRepo.undeliverTask(taskTreeItem, context)),
     commands.registerCommand(commandRepo.commands.storyState.resolveBlocker, blockerTreeItem => commandRepo.resolveBlocker(blockerTreeItem, context)),
