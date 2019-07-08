471831e7546a591b20874aea23e48459fa85ad7e
Brian Muthui <brian.muthui@andela.com>
2019-07-08 20:15:44 +0300

    Add and fix tests

--- model/iterations/index.spec.js
+++ model/iterations/index.spec.js
@@ -10,7 +10,7 @@ const IterationResource = new PtIteration(context)
 describe('#iterations', () => {
   describe('_endpoints', () => {
     const modelMockInstance = Model.mock.instances[0]
-    modelMockInstance._addParams = jest.fn().mockReturnValue('apiPath?params=7')
+    modelMockInstance._addParams.mockReturnValue('apiPath?params=7')
 
     afterEach(() => Model.mockClear())
 
@@ -43,7 +43,9 @@ describe('#iterations', () => {
 
   describe('getIterations', () => {
     const modelMockInstance = Model.mock.instances[0]
-    modelMockInstance._addParams = jest.fn().mockReturnValue('apiPath?params=7')
+    modelMockInstance._addParams.mockReturnValue('apiPath?params=7')
+    const mockData = {data:[{stories: [{name: 'story 1', id: 1}]}]}
+    modelMockInstance._fetch.mockImplementation(() => mockData)
 
     afterEach(() => Model.mockClear())
 
@@ -55,34 +57,56 @@ describe('#iterations', () => {
       await IterationResource.getIterations('current')
       expect(modelMockInstance._fetch).toHaveBeenCalledWith('get', 'apiPath?params=7')
     })
+
+    it('should not compress data if compress is false', async () => {
+      const iterations = await IterationResource.getIterations('current', false)
+      expect(iterations).toMatchObject(mockData)
+    })
+
+    it('should compress data if compress is true', async () => {
+      const iterations = await IterationResource.getIterations('current', true)
+      expect(iterations).toMatchObject([`${mockData.data[0].stories[0].name} - ${mockData.data[0].stories[0].id}`])
+    })
   })
 
   describe('getDoneIterations', () => {
     const modelMockInstance = Model.mock.instances[0]
-    modelMockInstance._addParams = jest.fn().mockReturnValue('apiPath?params=7')
+    modelMockInstance._addParams.mockReturnValue('apiPath?params=7')
+
+    const mockData = {data:[{stories: [{name: 'story 1', id: 1}]}]}
+    modelMockInstance._fetch.mockImplementation(() => mockData)
 
     afterEach(() => Model.mockClear())
+
+    test('it should raise error if scope is invalid', async () => {
+      await expect(IterationResource.getDoneIterations('invalid')).rejects.toThrowError('Invalid scope')
+    })
+
+    test('it should call fetch with the right params', async () => {
+      await IterationResource.getDoneIterations('done')
+      expect(modelMockInstance._fetch).toHaveBeenCalledWith('get', 'apiPath?params=7')
+    })
+
+    
+    it('should not compress data if compress is false', async () => {
+      const iterations = await IterationResource.getDoneIterations('done', 1000, false)
+      expect(iterations).toMatchObject(mockData)
+    })
+
+    it('should compress data if compress is true', async () => {
+      const iterations = await IterationResource.getDoneIterations('done', 1000, true)
+      expect(iterations).toMatchObject([`${mockData.data[0].stories[0].name} - ${mockData.data[0].stories[0].id}`])
+    })
 
     test('it should call getIterations if scope does not include done', () => {
       IterationResource.getIterations = jest.fn()
       IterationResource.getDoneIterations('current')
       expect(IterationResource.getIterations).toHaveBeenCalledTimes(1)
     })
-
-    test('it should raise error if scope is invalid', async () => {
-      await expect(IterationResource.getDoneIterations('invalid')).rejects.toThrowError('Invalid scope')
-    })
-
-
-    test('it should call fetch with the right params', async () => {
-      await IterationResource.getDoneIterations('done')
-      expect(modelMockInstance._fetch).toHaveBeenCalledWith('get', 'apiPath?params=7')
-    })
   })
 
   describe('getIterationCycleTime', () => {
     const modelMockInstance = Model.mock.instances[0]
-    modelMockInstance._baseApiPath = 'apiPath'
 
     afterEach(() => Model.mockClear())
 
